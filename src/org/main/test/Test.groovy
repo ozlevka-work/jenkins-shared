246aa8ce87107d3d4394d298e5d191416cb8042d
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-05-19 17:39:43 +0300

    Test Groovy features in Jenkins pipeline

--- src/org/main/test/Test.groovy
+++ src/org/main/test/Test.groovy
@@ -5,11 +5,23 @@ package org.main.test
  */
 class Test {
 
+
+
     def removeNewLine(String text) {
         if(text == null || text.isEmpty()) {
             return text
         } else {
             return text.replaceAll(/\n/, "<br/>")
         }
+    }
+
+
+    def tryGroovyFeature(String text) {
+        def strArr = []
+        (1..20).each {
+            strArr << it.toString() + "Hello \n"
+        }
+
+        return strArr
     }
 }
