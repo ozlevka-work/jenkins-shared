a0373d01598e64471006bc482940bc4cbbf96f6a
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-06-06 22:21:27 +0300

    changes to save

--- src/com/ericom/jenkins/PipeLine.groovy
+++ src/com/ericom/jenkins/PipeLine.groovy
@@ -12,6 +12,8 @@ class PipeLine implements Serializable {
     def currentBuild
     def config
 
+    def changeset = [:]
+
     PipeLine(steps, current) {
         this.steps = steps
         this.currentBuild = current
@@ -23,6 +25,33 @@ class PipeLine implements Serializable {
     }
 
     def run() {
+        this.steps.stage("Fetch changes") {
+            this.steps.git([url: this.config['svc']['url'], credentialsId: this.config['credentilas']['git'], changelog: true])
+            def changeLogSets = this.currentBuild.rawBuild.changeSets
+        }
+    }
 
+
+    def makeChangeset(changeLogSets) {
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                def files = new ArrayList(entry.affectedFiles)
+                for (int k = 0; k < files.size(); k++) {
+                    def file = files[k]
+                    this.findComponent(file.path)
+                }
+            }
+        }
+    }
+
+    def findComponent(String path) {
+        def lst = this.config['components'].keySet() as List
+        for(def key in lst) {
+            if(path.startsWith(this.config['components'][key]['path'].toString())) {
+                this.changeset[key] = true
+            }
+        }
     }
 }
--- src/main.groovy
+++ src/main.groovy
@@ -15,10 +15,56 @@ def testLoadConfig() {
 
     pl.loadConfig(cofStr)
     println pl.config
+
+    return pl
+}
+
+
+def testConfig() {
+    def pl = testLoadConfig()
+
+    assert pl.config['credentilas']['git'] == 'ozlevka-github'
+}
+
+def testArray = [
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.ko-kr.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/src/accessnow.min.js.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.ja-jp.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/AvenirNextLTPro-Bold.ttf.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.de.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.zh-cn.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.fr.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/FontAwesome.otf.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.ru.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.zh-tw.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/AvenirNextLTPro-Bold.woff.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.en-us.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/fontawesome-webfont.eot.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.it.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.list.txt.gz",
+        "AccessNow/css/shield.css",
+        "Containers/Docker/shield-icap/web/AccessNow/css/accessnow.min.css",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/AvenirNextLTPro-Bold.svg.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/fontawesome-webfont.svg.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/AvenirNextLTPro-Bold.eot.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.pt-br.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/css/accessnow.min.css.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/fontawesome-webfont.woff2.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/fontawesome-webfont.ttf.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/dictionary.es-ar.txt.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/src/vendor.min.js.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/fontawesome-webfont.woff.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/resources/lang/12Dec2012ANChineseSimplified.lang.gz",
+        "Containers/Docker/shield-icap/web/AccessNow/fonts/AvenirNextLTPro-Bold.min.svg.gz",
+        "Containers/Docker/shield-configuration/Dockerfile"
+]
+
+def testChangesFinding() {
+
 }
 
 
 
-testLoadConfig()
+testConfig()
 
 
