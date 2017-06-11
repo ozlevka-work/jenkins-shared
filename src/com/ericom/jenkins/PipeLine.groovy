e5241dbc14a17b2b3fc4e4947fac23b2dc0c6fa1
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-06-11 19:43:07 +0300

    Build stage

--- src/com/ericom/jenkins/PipeLine.groovy
+++ src/com/ericom/jenkins/PipeLine.groovy
@@ -25,6 +25,15 @@ class PipeLine implements Serializable {
     }
 
     def run() {
+        if (fetchChangesCodeChanges()) {
+
+        } else {
+            this.steps.echo "No changes in code found"
+        }
+    }
+
+
+    def fetchChangesCodeChanges() {
         this.steps.stage("Fetch changes") {
             if (this.config["svc"].containsKey('branch')) {
                 this.steps.git([url: this.config['svc']['url'], credentialsId: this.config['credentilas']['git'], branch: this.config['svc']['branch'], changelog: true])
@@ -46,31 +55,60 @@ class PipeLine implements Serializable {
                     }
                 }
             }
-
-            this.steps.echo this.changeset.toString()
         }
+
+        return this.changeset.size() > 0
     }
 
 
-    def makeChangeset(changeLogSets) {
-        for (int i = 0; i < changeLogSets.size(); i++) {
-            def entries = changeLogSets[i].items
-            for (int j = 0; j < entries.length; j++) {
-                def entry = entries[j]
-                def files = new ArrayList(entry.affectedFiles)
-                for (int k = 0; k < files.size(); k++) {
-                    def file = files[k]
-                    this.findComponent(file.path)
-                }
-            }
-        }
-    }
+//    def makeChangeset(changeLogSets) {
+//        for (int i = 0; i < changeLogSets.size(); i++) {
+//            def entries = changeLogSets[i].items
+//            for (int j = 0; j < entries.length; j++) {
+//                def entry = entries[j]
+//                def files = new ArrayList(entry.affectedFiles)
+//                for (int k = 0; k < files.size(); k++) {
+//                    def file = files[k]
+//                    this.findComponent(file.path)
+//                }
+//            }
+//        }
+//    }
 
     def findComponent(String path) {
         def lst = this.config['components'].keySet() as List
         for(def key in lst) {
             if(path.startsWith(this.config['components'][key]['path'].toString())) {
                 this.changeset[key] = true
+            }
+        }
+    }
+
+    def makeDependencies() {
+        def needed_to_build = []
+        def keys = this.changeset.keySet()
+
+        for(int i = 0; i < keys.size(); i++) {
+            def key = keys[i]
+            needed_to_build.add(key)
+            if(this.config['components'][key].containsKey('dependency')) {
+                for(int j = 0; j < this.config['components'][key]['dependency'].size(); j++) {
+                    needed_to_build.add(this.config['components'][key]['dependency'][j])
+                }
+            }
+        }
+
+        return needed_to_build
+    }
+
+
+    def runBuildForChanged() {
+        this.steps.stage('Build Images') {
+            def build_array = makeDependencies()
+            for(i = 0; i < build_array.size(); i++) {
+                def buildPath = this.config['components'][build_array[i]]['path']
+                this.steps.sh "cd ${buildPath} && sudo ./_build.sh"
+                this.steps.echo "Component ${build_array[i]} succesfully build"
             }
         }
     }
