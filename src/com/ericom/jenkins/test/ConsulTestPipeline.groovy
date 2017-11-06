c922613bf7a72b07911ba38e970c573aaae49333
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-11-06 14:40:36 +0200

    Implement library

--- src/com/ericom/jenkins/test/ConsulTestPipeline.groovy
+++ src/com/ericom/jenkins/test/ConsulTestPipeline.groovy
@@ -1,15 +1,20 @@
 package com.ericom.jenkins.test
-
 import com.ericom.jenkins.PipelineBase
 
+@Grab(group = 'org.yaml', module='snakeyaml', version = "1.18")
+import org.yaml.snakeyaml.Yaml
+
 class ConsulTestPipeline extends PipelineBase{
+    def consul_run_config
 
     ConsulTestPipeline(steps, current, environment) {
         super(steps, current, environment)
     }
 
     def downloadYamlFile() {
+        def yaml = new Yaml()
         this.steps.sh "wget ${this.config['files']['branchUrl']}${this.config['files']['yaml']}"
+        this.consul_run_config = yaml.load("./${this.config['files']['yaml']}")
     }
 
     def runSystem() {
@@ -17,10 +22,15 @@ class ConsulTestPipeline extends PipelineBase{
         this.steps.sh "docker stack deploy -c ./${this.config['files']['yaml']} shield"
     }
 
+    def createNewTag() {
+
+    }
+
     def run() {
         this.steps.stage('Setup consul') {
             this.downloadYamlFile()
             this.runSystem()
+            this.steps.echo this.consul_run_config["services"]["consul-server"]["image"]
         }
     }
 }
