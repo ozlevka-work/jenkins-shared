086992c06d2983edf7b079846821e1f95b91eccc
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2017-12-28 11:59:51 +0200

    Add Grab to work

--- src/com/ericom/jenkins/test/EricomYamlParser.groovy
+++ src/com/ericom/jenkins/test/EricomYamlParser.groovy
@@ -1,8 +1,8 @@
 package com.ericom.jenkins.test
-
+@Grab(group='org.yaml', module='snakeyaml', version='1.14')
 import org.yaml.snakeyaml.Yaml
 
-class EricomYamlParser {
+class EricomYamlParser implements Serializable {
 
     EricomYamlParser(String originalFile) {
         println(originalFile)
