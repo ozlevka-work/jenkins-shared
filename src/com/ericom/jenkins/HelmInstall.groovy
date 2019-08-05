0db6aff58d0fc961ecd2e89291fe83beac69fa52
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2019-08-05 10:37:09 +0300

    Start implement HelmInstall

--- src/com/ericom/jenkins/HelmInstall.groovy
+++ src/com/ericom/jenkins/HelmInstall.groovy
@@ -3,4 +3,16 @@ package com.ericom.jenkins
 class HelmInstall implements Serializable {
     def steps
     def currentBuild
+    def builParams
+
+    HelmInstall(steps, currentBuild, buidParams) {
+        this.steps = steps
+        this.currentBuild = currentBuild
+
+        this.builParams = steps.readJSON text: buidParams
+    }
+
+    def checkParams() {
+        this.steps.echo "${this.builParams.getClass().getName()}"
+    }
 }
