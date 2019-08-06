d7be329c1d29a2c6280bad71f6daeb941bea48e6
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2019-08-06 10:12:46 +0300

    Try print template

--- src/com/ericom/jenkins/HelmInstall.groovy
+++ src/com/ericom/jenkins/HelmInstall.groovy
@@ -70,12 +70,12 @@ class HelmInstall implements Serializable {
     }
 
     def runCommandSplitOutputAndRun(command, template) {
+        this.steps.echo "${cmd}"
         def commandOutput = this.steps.sh script: command, returnStdout: true
         def command_out_array = commandOutput.split('\n')
         if (command_out_array.size() > 0 && command_out_array[0] != '') {
             for(def el : command_out_array) {
                 def cmd = template.format(el)
-                this.steps.echo "${cmd}"
                 this.steps.sh script: cmd
             }
         }
